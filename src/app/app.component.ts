cba44f32e05629263a9deae07ecd81c6f1177d5e
Nains <jason20226@gmail.com>
2021-02-09 14:20:11 +0800

    2021-02-09-完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+    $(document).ready(function () {
+      $("#example").textition({
+        speed: 1.3,
+        animation: "ease-in-out",
+        map: { x: 100, y: 100, z: 0 },
+        autoplay: true,
+        interval: 2.2,
+      });
+    });
+
+  }
 }
